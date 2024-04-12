d8d159f8651ad0fdffb6d44de4a6486a641c10d6
tristan <tristan.hacquard@polytechnique.org>
2024-04-12 14:59:55 +0200

    fixed visualisation

--- src/MLNanoShaper.jl
+++ src/MLNanoShaper.jl
@@ -4,15 +4,25 @@ using StructArrays
 using GeometryBasics: Sphere
 using GLMakie
 
+
 function Base.read(io::IO, ::Type{Sphere{T}}) where {T}
-	Sphere([read(io, T), read(io, T), read(io, T)], read(io, T))
+	line = readline(io)
+	x,y,z,r = parse.(T,split(line))
+	Sphere(Point3(x,y,z),r)
 end
-function Base.read(io::IO, ::Type{StructArray{Sphere{T}}}) where {T}
-    StructArray{Sphere}(read.(io, Sphere{T}))
+function Base.read(io::IO, ::Type{Vector{Sphere{T}}}) where {T}
+	out = Sphere{T}[]
+	while !eof(io)
+		push!(out,read(io, Sphere{T}))
+	end
+	out
 end
 
-mesh(Sphere(Point3(0,0,0),1))
-
-
+function viz(x::AbstractVector)
+	fig  = Figure()
+	ax = Axis(fig[1,1])
+	mesh!.(Ref(ax),x)
+	fig
+end
 
 end
