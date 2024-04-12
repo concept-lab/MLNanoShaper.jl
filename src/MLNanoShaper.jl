f9fe84f8b7296fef06e6a2d3efe5b91ed4ba2b7b
tristan <tristan.hacquard@polytechnique.org>
2024-04-12 12:07:17 +0200

    added support for xyzr files

--- src/MLNanoShaper.jl
+++ src/MLNanoShaper.jl
@@ -1,5 +1,18 @@
 module MLNanoShaper
 
-# Write your package code here.
+using StructArrays
+using GeometryBasics: Sphere
+using GLMakie
+
+function Base.read(io::IO, ::Type{Sphere{T}}) where {T}
+	Sphere([read(io, T), read(io, T), read(io, T)], read(io, T))
+end
+function Base.read(io::IO, ::Type{StructArray{Sphere{T}}}) where {T}
+    StructArray{Sphere}(read.(io, Sphere{T}))
+end
+
+mesh(Sphere(Point3(0,0,0),1))
+
+
 
 end
