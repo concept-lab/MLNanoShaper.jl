3ab0ac5f43f9ef17e66ff048661655fda6ba2f16
tristan <tristan.hacquard@polytechnique.org>
2024-04-16 15:28:53 +0200

    added export to xyzr

--- src/MLNanoShaper.jl
+++ src/MLNanoShaper.jl
@@ -1,28 +1,37 @@
 module MLNanoShaper
+export extract_balls
 
 using StructArrays
-using GeometryBasics: Sphere
 using GLMakie
+using GeometryBasics
+using BioStructures
 
+struct XYZR{T} end
+struct DICT{T} end
 
-function Base.read(io::IO, ::Type{Sphere{T}}) where {T}
-	line = readline(io)
-	x,y,z,r = parse.(T,split(line))
-	Sphere(Point3(x,y,z),r)
+function read_sphere(io::IO, ::Type{Sphere{T}}) where {T}
+    line = readline(io)
+    x, y, z, r = parse.(T, split(line))
+    Sphere(Point3(x, y, z), r)
 end
-function Base.read(io::IO, ::Type{Vector{Sphere{T}}}) where {T}
-	out = Sphere{T}[]
-	while !eof(io)
-		push!(out,read(io, Sphere{T}))
-	end
-	out
-end
-
-function viz(x::AbstractVector)
-	fig  = Figure()
-	ax = Axis(fig[1,1])
-	mesh!.(Ref(ax),x)
-	fig
+function Base.read(io::IO, ::XYZR{T}) where {T}
+    out = Sphere{T}[]
+    while !eof(io)
+        push!(out, read_sphere(io, Sphere{T}))
+    end
+    out
 end
 
+function viz(x::AbstractVector{Sphere{T}}) where {T}
+    fig = Figure()
+    ax = Axis3(fig[1, 1])
+    mesh!.(Ref(ax), x)
+    fig
+end
+
+repeat(f, n::Integer) = foldr(∘, Iterators.repeated(f, n))
+function extract_balls(prot::ProteinStructure)
+    recurse(fun, arg) = mapreduce(vcat, fun, arg)
+    repeat(x -> recurse(atom -> Point3(atom.coord...), x), 4)(prot)
+end
 end
