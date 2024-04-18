a21a889e823f903c7f6c8efe3888b63917ff8ec7
tristan <tristan.hacquard@polytechnique.org>
2024-04-18 16:33:44 +0200

    added data generation

---
+++ src/generate_data.jl
@@ -0,0 +1,28 @@
+using TOML
+using BioStructures
+using Logging
+using MLNanoShaper
+
+function generate_data()
+	data_dir = "$(homedir())/datasets/proteins"
+    params = TOML.parsefile("param/param.toml")
+    proteins = downloadpdb(params["protein"]["list"])
+	radii = read("param/protein.r.dict",MLNanoShaper.DICT{Float64})
+	project_dir=pwd()
+
+    cd(mktempdir(prefix = "nanoshaper")) do
+		cp("$project_dir/param/conf.prm", "conf.prm")
+        for prot_path in proteins
+            _, prot_name = splitdir(prot_path)
+            mesh_name = first(split(prot_name, ".")) * ".off"
+            @info "generating surface" mesh_name
+            prot = read(prot_path, PDB)
+            atoms = extract_balls(prot, radii)
+            open("atoms.xyzr","w") do io
+                print(io, atoms, MLNanoShaper.XYZR{Float64})
+            end
+			run(pipeline(`Nanoshaper conf.prm`,devnull))
+            cp("triangulatedSurf.off", "$data_dir/$mesh_name",force = true)
+        end
+    end
+end
