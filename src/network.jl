362e894697c3a5333188cdfc48fc740b4d1c9289
tristan <tristan.hacquard@polytechnique.org>
2024-05-07 11:45:02 +0200

    fix: fixed boundry for marching cube

--- src/network.jl
+++ src/network.jl
@@ -123,7 +123,8 @@ function test((; atoms, skin)::TrainingData{Float32},
         @info "test" loss stats
     end
 
-	surface = isosurface()do x
+    (; mins, maxes) = atoms.hyper_rec
+	surface = isosurface(;origin=mins,width=maxes-mins)do x
         atoms_neighboord = atoms[inrange(atoms_tree, x, r)] |> StructVector
 		if length(atoms_neighboord) >= 0
 			training_states.model(atoms_neighboord,training_states.parameters,training_states.states) |> first
