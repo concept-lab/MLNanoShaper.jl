eaca50d9b4110cc21b84518868e44e58df2ea36c
tristan <tristan.hacquard@polytechnique.org>
2024-05-15 18:10:00 +0200

    feature: added name in cli

--- src/cli.jl
+++ src/cli.jl
@@ -1,5 +1,5 @@
 using Comonicon
-Option{T} = Union{T,Nothing} 
+Option{T} = Union{T, Nothing}
 """
     train
 
@@ -12,22 +12,25 @@ The folowing parameters can be overided.
 - `-m, --model=String`; the model name. Can be anakin.
 
 """
-@main function train(;nb_epoch::Option{UInt}=nothing,model::Option{String}=nothing,nb_data_points::Option{UInt}=nothing)
-	conf = TOML.parsefile(params_file)
-	if !isnothing(nb_epoch)	
-		conf["Training_parameters"]["nb_epoch"] = nb_epoch
-	end
-	if !isnothing(model)	
-		conf["Training_parameters"]["model"] = model
-	end
-	if !isnothing(nb_data_points)	
-		conf["Training_parameters"]["data_ids"] =conf["Training_parameters"]["data_ids"][begin:begin+nb_data_points] 
-	end
+@main function train(; nb_epoch::Option{UInt} = nothing, model::Option{String} = nothing,
+	nb_data_points::Option{UInt} = nothing,name::Option{String} = nothing)
+    conf = TOML.parsefile(params_file)
+    if !isnothing(nb_epoch)
+        conf["Training_parameters"]["nb_epoch"] = nb_epoch
+    end
+    if !isnothing(name)
+		conf["Training_parameters"]["name"] = name
+    end
+    if !isnothing(model)
+        conf["Training_parameters"]["model"] = model
+    end
+    if !isnothing(nb_data_points)
+        conf["Training_parameters"]["data_ids"] = conf["Training_parameters"]["data_ids"][begin:(begin + nb_data_points)]
+    end
 
-	training_parameters = read_from_TOML(Training_parameters,conf)
-	auxiliary_parameters = read_from_TOML(Auxiliary_parameters,conf)
-	@info "Starting training"
-	train(training_parameters,auxiliary_parameters)
-	@info "Stop training"
-
+    training_parameters = read_from_TOML(Training_parameters, conf)
+    auxiliary_parameters = read_from_TOML(Auxiliary_parameters, conf)
+    @info "Starting training"
+    train(training_parameters, auxiliary_parameters)
+    @info "Stop training"
 end
