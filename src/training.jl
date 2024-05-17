285d182717c202bbf153a2790a8f5790273cb200
tristan <tristan.hacquard@polytechnique.org>
2024-05-17 10:51:14 +0200

    fix: added assert for hausdorff distance

--- src/training.jl
+++ src/training.jl
@@ -130,8 +130,9 @@ end
 
 function hausdorff_metric((; atoms, atoms_tree, skin)::TreeTrainingData,
         training_states::Lux.Experimental.TrainState, training_parameters::Training_parameters)
-    surface = implicit_surface(atoms_tree, atoms, training_states, training_parameters)
-    distance(first(surface), skin.tree)
+    surface = implicit_surface(atoms_tree, atoms, training_states, training_parameters) |> first
+	@assert length(surface) >= 1
+    distance(surface, skin.tree)
 end
 
 """
