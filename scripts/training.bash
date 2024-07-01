49ff016605d3a1ee22ed297b4e00fd2eec704cec
tristan <tristan.hacquard@polytechnique.org>
2024-07-01 16:48:38 +0200

    feature: added training script

---
+++ scripts/training.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+batch_name=small_grid_3
+parallel --jobs 4\
+	~/.julia/bin/mlnanoshaper train\
+		--model={1}\
+		--cutoff-radius={2}\
+		--learning-rate={3}\
+		--name={2}A_$small_grid_3\
+		--model-kargs.van_der_wal_channel {4}\
+		--categorical {5}\
+	::: tiny_angular_dense light_angular_dense medium_andular_dense\
+	::: 2.0\
+	::: 1e-5\
+ 	::: true false\
+	::: true false
+
+parallel --jobs 3\
+	~/.julia/bin/mlnanoshaper train\
+		--model={1}\
+		--cutoff-radius={2}\
+		--learning-rate={3}\
+		--name={2}A_$batch_name\
+		--model-kargs.van_der_wal_channel {4}\
+		--categorical {5}\
+	::: tiny_angular_dense \
+	::: 3.0\
+	::: 2e-6\
+ 	::: true false\
+	::: true false
+
+parallel --jobs 2\
+	~/.julia/bin/mlnanoshaper train\
+		--model={1}\
+		--cutoff-radius={2}\
+		--learning-rate={3}\
+		--name={2}A_$batch_name\
+		--model-kargs.van_der_wal_channel {4}\
+		--categorical {5}\
+	::: light_angular_dense medium_andular_dense\
+	::: 3.0\
+	::: 2e-6\
+ 	::: true false\
+	::: true false
+
+
+ parallel --jobs 1\
+	 ~/.julia/bin/mlnanoshaper train\
+		 --model={1}\
+		 --cutoff-radius={2}\
+		 --learning-rate={3}\
+		 --name={2}A_$batch_name\
+		 --model-kargs.van_der_wal_channel{4}\
+		 --categorical {5}\
+	::: tiny_angular_dense light_angular_dense medium_angular_dense\
+	::: 4.0\
+	::: 1e-7\
+	::: true false\
+	:::  true false
