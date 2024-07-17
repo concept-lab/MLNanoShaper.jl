0071a34860cf49173fc30ad6fb21c9ba9d913224
Documenter.jl <documenter@juliadocs.github.io>
2024-07-17 14:10:46 +0000

    build based on dbb314e

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -4,10 +4,8 @@ requirejs.config({
     'highlight-julia': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/julia.min',
     'headroom': 'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.12.0/headroom.min',
     'jqueryui': 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min',
-    'katex-auto-render': 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.8/contrib/auto-render.min',
     'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min',
     'headroom-jquery': 'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.12.0/jQuery.headroom.min',
-    'katex': 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.8/katex.min',
     'highlight': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min',
     'highlight-julia-repl': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/julia-repl.min',
   },
@@ -15,11 +13,6 @@ requirejs.config({
   "highlight-julia": {
     "deps": [
       "highlight"
-    ]
-  },
-  "katex-auto-render": {
-    "deps": [
-      "katex"
     ]
   },
   "headroom-jquery": {
@@ -36,32 +29,39 @@ requirejs.config({
 }
 });
 ////////////////////////////////////////////////////////////////////////////////
-require(['jquery', 'katex', 'katex-auto-render'], function($, katex, renderMathInElement) {
-$(document).ready(function() {
-  renderMathInElement(
-    document.body,
-    {
-  "delimiters": [
-    {
-      "left": "$",
-      "right": "$",
-      "display": false
-    },
-    {
-      "left": "$$",
-      "right": "$$",
-      "display": true
-    },
-    {
-      "left": "\\[",
-      "right": "\\]",
-      "display": true
-    }
-  ]
+require([], function() {
+window.MathJax = {
+  "tex": {
+    "packages": [
+      "base",
+      "ams",
+      "autoload"
+    ],
+    "inlineMath": [
+      [
+        "$",
+        "$"
+      ],
+      [
+        "\\(",
+        "\\)"
+      ]
+    ],
+    "tags": "ams"
+  },
+  "options": {
+    "ignoreHtmlClass": "tex2jax_ignore",
+    "processHtmlClass": "tex2jax_process"
+  }
 }
+;
 
-  );
-})
+(function () {
+    var script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg.js';
+    script.async = true;
+    document.head.appendChild(script);
+})();
 
 })
 ////////////////////////////////////////////////////////////////////////////////
@@ -485,6 +485,14 @@ function worker_function(documenterSearchIndex, documenterBaseURL, filters) {
   }
 
   /**
+   * RegX escape function from MDN
+   * Refer: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+   */
+  function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+  }
+
+  /**
    * Make the result component given a minisearch result data object and the value
    * of the search input as queryString. To view the result object structure, refer:
    * https://lucaong.github.io/minisearch/modules/_minisearch_.html#searchresult
@@ -502,8 +510,8 @@ function worker_function(documenterSearchIndex, documenterBaseURL, filters) {
     if (result.page !== "") {
       display_link += ` (${result.page})`;
     }
-
-    let textindex = new RegExp(`${querystring}`, "i").exec(result.text);
+    searchstring = escapeRegExp(querystring);
+    let textindex = new RegExp(`${searchstring}`, "i").exec(result.text);
     let text =
       textindex !== null
         ? result.text.slice(
@@ -520,7 +528,7 @@ function worker_function(documenterSearchIndex, documenterBaseURL, filters) {
     let display_result = text.length
       ? "..." +
         text.replace(
-          new RegExp(`${escape(querystring)}`, "i"), // For first occurrence
+          new RegExp(`${escape(searchstring)}`, "i"), // For first occurrence
           '<span class="search-result-highlight py-1">$&</span>'
         ) +
         "..."
@@ -566,6 +574,7 @@ function worker_function(documenterSearchIndex, documenterBaseURL, filters) {
         // Only return relevant results
         return result.score >= 1;
       },
+      combineWith: "AND",
     });
 
     // Pre-filter to deduplicate and limit to 200 per category to the extent
