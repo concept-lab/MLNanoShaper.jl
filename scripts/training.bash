acd0b03f717cbde5bf147bf12f6e743ccf33a1e8
tristan <tristan.hacquard@polytechnique.org>
2024-07-30 15:32:27 +0200

    feature: new training

--- scripts/training.bash
+++ scripts/training.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-batch_name=smooth_9
+batch_name=smooth_10
 echo training $batch_name
 
 parallel --jobs 3\
@@ -12,7 +12,7 @@ parallel --jobs 3\
 		--loss {5}\
 	::: tiny_angular_dense \
 	::: 3.0\
-	::: 1e-8\
+	::: 5e-8\
  	::: false\
 	::: categorical continuous
 
